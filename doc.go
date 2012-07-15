24f96d3c6127946f2e3390bc9d76d7babe487c37
Damian Gryski <damian@gryski.com>
2012-07-15 19:40:42 +0200

    package documentation

---
+++ doc.go
@@ -0,0 +1,13 @@
+/*
+Package dkrcrypt is an implementation of ciphers from Korea Internet Security Agency.
+
+Copyright (c) Damian Gryski <damian@gryski.com>
+Licensed under the GPLv3 or, at your option, any later version.
+
+The library includes HIGHT, SEED, and ARIA.
+
+These ciphers are used almost exclusively inside Korea.
+
+For more information on these ciphers, please see: http://seed.kisa.or.kr/kor/main.jsp
+*/
+package dkrcrypt
